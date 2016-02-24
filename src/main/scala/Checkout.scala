4be98cc0ca04c97a72ef455d9b6138705b422915
Barrie McGuire <barrie@bheap.co.uk>
2016-02-24 13:06:43 +0000

    step 1 - initial checkout system

--- src/main/scala/Checkout.scala
+++ src/main/scala/Checkout.scala
@@ -1,5 +1,12 @@
 package org.hmrc.checkout
 
 object Checkout {
-  def calculateBasketValue(items: Seq[String]): BigDecimal = ???
+  def calculateBasketValue(items: Seq[String]): BigDecimal = {
+    val prices = items.map {
+      case "apple" => BigDecimal("0.60")
+      case "orange" => BigDecimal("0.25")
+      case _ => BigDecimal("0")
+    }
+    prices.foldLeft(BigDecimal("0")) { _ + _ }
+  }
 }
--- src/test/scala/CheckoutSpec.scala
+++ src/test/scala/CheckoutSpec.scala
@@ -5,8 +5,19 @@ import org.scalatest.{WordSpec, Matchers}
 class CheckoutSpec extends WordSpec with Matchers {
 
   "Checkout" should {
-    "do something" in {
-      1 shouldBe 1
+    "Calculate the basket value for 1 apple and 1 orange" in {
+      val basketValue = Checkout.calculateBasketValue(Seq("apple", "orange"))
+      basketValue shouldBe BigDecimal("0.85")
+    }
+
+    "Calculate the basket value for 3 oranges" in {
+      val basketValue = Checkout.calculateBasketValue(Seq("orange", "orange", "orange"))
+      basketValue shouldBe BigDecimal("0.75")
+    }
+
+    "Calculate the basket value for an empty basket" in {
+      val basketValue = Checkout.calculateBasketValue(Nil)
+      basketValue shouldBe BigDecimal("0.00")
     }
   }
 
