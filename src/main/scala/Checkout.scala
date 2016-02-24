ee51c3652b87dff8b70fb9dc0fbe03973f194428
Barrie McGuire <barrie@bheap.co.uk>
2016-02-24 14:36:05 +0000

    step 2 - add support for offers

--- src/main/scala/Checkout.scala
+++ src/main/scala/Checkout.scala
@@ -1,12 +1,32 @@
 package org.hmrc.checkout
 
+trait Item {
+  val price: BigDecimal
+}
+
+case object Apple extends Item {
+  val price = BigDecimal("0.60")
+}
+
+case object Orange extends Item {
+  val price = BigDecimal("0.25")
+}
+
 object Checkout {
-  def calculateBasketValue(items: Seq[String]): BigDecimal = {
-    val prices = items.map {
-      case "apple" => BigDecimal("0.60")
-      case "orange" => BigDecimal("0.25")
-      case _ => BigDecimal("0")
-    }
-    prices.foldLeft(BigDecimal("0")) { _ + _ }
+  def calculateBasketValue(items: Seq[Item]): BigDecimal = {
+    val apples = items.collect { case apple @ Apple => apple }
+    val oranges = items.collect { case orange @ Orange => orange }
+    val itemsAfterOffers = buyOneGetOneFree(apples) ++ buyTwoGetOneFree(oranges)
+    itemsAfterOffers.map(_.price).foldLeft(BigDecimal("0")) { _ + _ }
+  }
+
+  def buyOneGetOneFree(items: Seq[Item]): Seq[Item] = {
+    val numberOfItemsAfterOffer = (items.size / 2) + (items.size % 2)
+    items.take(numberOfItemsAfterOffer)
+  }
+
+  def buyTwoGetOneFree(items: Seq[Item]): Seq[Item] = {
+    val numberOfItemsAfterOffer = ((items.size / 3) * 2) + (items.size % 3)
+    items.take(numberOfItemsAfterOffer)
   }
 }
--- src/test/scala/CheckoutSpec.scala
+++ src/test/scala/CheckoutSpec.scala
@@ -6,13 +6,24 @@ class CheckoutSpec extends WordSpec with Matchers {
 
   "Checkout" should {
     "Calculate the basket value for 1 apple and 1 orange" in {
-      val basketValue = Checkout.calculateBasketValue(Seq("apple", "orange"))
+      val basketValue = Checkout.calculateBasketValue(Seq(Apple, Orange))
       basketValue shouldBe BigDecimal("0.85")
     }
 
     "Calculate the basket value for 3 oranges" in {
-      val basketValue = Checkout.calculateBasketValue(Seq("orange", "orange", "orange"))
-      basketValue shouldBe BigDecimal("0.75")
+      val basketValue = Checkout.calculateBasketValue(Seq(Orange, Orange, Orange))
+      basketValue shouldBe BigDecimal("0.50")
+    }
+
+    "Calculate the basket value for 4 oranges and 3 apples" in {
+      val basketValue = Checkout.calculateBasketValue(Seq(Orange, Orange, Orange, Orange, Apple, Apple, Apple))
+      basketValue shouldBe BigDecimal("1.95")
+    }
+
+    "Calculate the basket value for 10 oranges and 8 apples" in {
+      val basketValue = Checkout.calculateBasketValue(Seq(Apple, Apple, Orange, Apple, Orange, Orange,
+        Orange, Orange, Orange, Orange, Orange, Apple, Orange, Orange, Apple, Apple, Apple, Apple))
+      basketValue shouldBe BigDecimal("4.15")
     }
 
     "Calculate the basket value for an empty basket" in {
